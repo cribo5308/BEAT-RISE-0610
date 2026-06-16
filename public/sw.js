f4140b986b8ec25e2437f863165ab4e7b72e7feb
cribo5308 <jykim5308@gmail.com>
2026-06-16 21:04:06 +0900

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,40 @@
+const CACHE_NAME = "beat-rise-v1";
+
+const urlsToCache = [
+  "/",
+  "/manifest.webmanifest",
+  "/audio/alex-morgan-future-bass-drop-537458.mp3"
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
 
